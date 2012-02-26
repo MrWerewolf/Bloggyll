fbf47c009584c35f39405412b3aa569a3e9d3c0b
Ryan Seto <mr.werewolf@gmail.com>
2012-02-25 19:15:51 -0500

    Add support for posts using directories as categories to postfiles

--- _plugins/postfiles.rb
+++ _plugins/postfiles.rb
@@ -19,18 +19,36 @@ module Jekyll
       end
 
       site.posts.each do |post|
+        year = post.date.strftime("%Y")
+        month = post.date.strftime("%m")
+        day = post.date.strftime("%d")
+
         # Go back to the single-file post name
-        postfile_id = post.id.gsub(/[\s\w\/]*(\d{4})\/(\d\d)\/(\d\d)\/(.*)/, '\1-\2-\3-\4')
+        postfile_id = "#{year}-#{month}-#{day}-#{post.slug}"
+
         # Get the directory that files from this post would be in
         postfile_dir = File.join(site.config['source'], '_postfiles', postfile_id)
 
         # Add a static file entry for each postfile, if any
-        Dir[File.join(postfile_dir, '/*')].each do |pf|
-          site.static_files << PostFile.new(site, postfile_dir, post.url, File.basename(pf))
-        end
+        addFilesToPost(site, postfile_dir, post)
+
+        # If categories are specified by directory, check that directory for postfiles
+        postfile_dir = File.join(
+          site.config['source'],
+          post.categories.join('/'),
+          '_postfiles',
+          postfile_id)
+
+        # Add the files from the category directory, if any
+        addFilesToPost(site, postfile_dir, post)
       end
     end
 
+    def addFilesToPost(site, dir, post)
+      Dir[File.join(dir, '/*')].each do |file|
+        site.static_files << PostFile.new(site, dir, post.url, File.basename(file))
+      end
+    end
   end
 
   class PostfileTag < Liquid::Tag
